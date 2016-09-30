2d4fa49683e854052281cb4bd6e9f38ff88c0776
Nightlamp1 <robert.britt2011@gmail.com>
2016-09-30 01:15:35 -0500

    Adding bootstrap files and basic rebel dropdown functionality

--- xwingv2/viewer/static/js/cardviewer.js
+++ xwingv2/viewer/static/js/cardviewer.js
@@ -1,5 +1,12 @@
 //pilots in pilots array
 
-for(i=0;i<pilots.length;i++){
-  console.log(pilots[i].faction);
-}
+
+$(document).ready(function(){
+  for(i=0;i<pilots.length;i++){
+    console.log(pilots[i].faction);
+    if(pilots[i].faction == "Rebel Alliance" || pilots[i].faction == "Resistance"){
+      $("#RebelPilots").append("<li><a>" + pilots[i].name + "</a></li>");
+      console.log("made it in if");
+    }
+  }
+});
