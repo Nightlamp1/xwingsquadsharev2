58cb063beb36f95ad508c9bee61eb813cccf5693
Nightlamp1 <robert.britt2011@gmail.com>
2016-09-30 01:50:32 -0500

    Basic functionality for selecting card to view

--- xwingv2/viewer/static/js/cardviewer.js
+++ xwingv2/viewer/static/js/cardviewer.js
@@ -2,11 +2,22 @@
 
 
 $(document).ready(function(){
+  //Iterate over pilot objects array to populate dropdowns
   for(i=0;i<pilots.length;i++){
-    console.log(pilots[i].faction);
+    //Populate html dropdowns with pilots from respective factions
+    var listString = "<li><a onclick=updateViewer("+pilots[i].id+ ");>" + pilots[i].name + "</a></li>";
     if(pilots[i].faction == "Rebel Alliance" || pilots[i].faction == "Resistance"){
-      $("#RebelPilots").append("<li><a>" + pilots[i].name + "</a></li>");
-      console.log("made it in if");
+      $("#RebelPilots").append(listString);
+    }else if(pilots[i].faction == "Galactic Empire" || pilots[i].faction == "First Order"){
+      $("#EmpirePilots").append(listString);
+    }else if(pilots[i].faction == "Scum and Villainy"){
+      $("#ScumPilots").append(listString);
     }
   }
 });
+
+function updateViewer(pilot){
+  var currentPilot = $.grep(pilots, function(e){ return e.id == pilot; });
+  console.log("Made it into function");
+  console.log(currentPilot[0].name);
+}
