7ed2efdcebdbf5fe2a778cdf8df90ba7ce0172d6
Nightlamp1 <robert.britt2011@gmail.com>
2016-10-09 05:07:04 -0500

    add pilot button basic functionality with accordian dropdowns

--- xwingv2/builder/static/js/squadbuilder.js
+++ xwingv2/builder/static/js/squadbuilder.js
@@ -1,35 +1,15 @@
 //pilots in pilots array
 
-
+var ships = {}
 $(document).ready(function(){
   //Generate ship list
-  var rebels = ["Rebel Alliance","Resistance"];
-  var empire = ["Galactic Empire", "First Order"];
-  var rebelShips = populateShipArray(pilots,rebels);
-  var empireShips = populateShipArray(pilots,empire);
-  var scumShips = populateShipArray(pilots,["Scum and Villainy"]);
-
-  //Iterate over pilot objects array to populate dropdowns
-  for(i=0;i<pilots.length;i++){
-    //Populate html dropdowns with pilots from respective factions
-    var listString = "<li><a onclick=selectPilot("+pilots[i].id+ ");>" + pilots[i].name + "</a></li>";
-    var shipID = pilots[i].ship.replace(/\s/g, '') + "pilots";
-    shipID = shipID.replace("(","");
-    shipID = shipID.replace(")","");
-    shipID = shipID.replace("/","");
-
-    if(pilots[i].faction == "Rebel Alliance" || pilots[i].faction == "Resistance"){
-      $("#" + shipID + "Rebels").append(listString);
-    }else if(pilots[i].faction == "Galactic Empire" || pilots[i].faction == "First Order"){
-      $("#" + shipID + "Empire").append(listString);
-    }else if(pilots[i].faction == "Scum and Villainy"){
-      $("#" + shipID + "Scum").append(listString);
-    }
-  }
+  ships.rebels = populateShipArray(pilots,["Rebel Alliance","Resistance"]);
+  ships.empire = populateShipArray(pilots,["Galactic Empire", "First Order"]);
+  ships.scum = populateShipArray(pilots,["Scum and Villainy"]);
 });
 
 function selectFaction(faction){
-  console.log(faction);
+  currentShips = ships[faction];
 }
 
 function populateShipArray(pilotArray,factions){
@@ -40,8 +20,4 @@ function populateShipArray(pilotArray,factions){
     }
   }
   return shipArray;
-}
-
-function selectPilot(pilot){
-  console.log(pilot);
 }
