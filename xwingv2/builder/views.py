285ec9082e2b2065bf7201b2e376ffe697995c84
Nightlamp1 <robert.britt2011@gmail.com>
2016-10-09 03:34:51 -0500

    Basic builder html setup and initial javascript file copied from viewer

--- xwingv2/builder/views.py
+++ xwingv2/builder/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
+import json
 
 # Create your views here.
 def builder(request):
-    return render(request,'builder/builder.html')
+    pilots = open(os.path.join(settings.STATIC_ROOT,'xwing-data/data/pilots.js'))
+    pilots = json.load(pilots)
+    return render(request,'builder/builder.html', {'pilots':json.dumps(pilots)})
